5604cdc784198145aaf019b30163322bcb5fa528
ogiamien emmanuel tammy <ogiamien222@gmail.com>
2023-11-26 17:28:57 +0100

    modified:   src/components/chatComponent/MainChat.jsx

--- src/components/chatComponent/MainChat.jsx
+++ src/components/chatComponent/MainChat.jsx
@@ -1,9 +1,28 @@
-import React, { useState, useEffect} from 'react'
 import menu from '../../assets/svg/menu.svg'
 import logo from '../../assets/svg/logoBlack.svg'
 import mainchatlogo from '../../assets/mainchatai.png'
+import { useState } from "react"
+import MessageBox from './MessageBox'
+import SideNav from './SideNav'
+import iconhead from '../../assets/iconhead.png'
 
-const MainChat = () => {
+const data = [
+  'I feel dizzy and I have a slight headache now and then',
+  'I feel dizzy and I have a slight headache now and then',
+  'I feel dizzy and I have a slight headache now and then',
+  'I feel dizzy and I have a slight headache now and then',
+  'I feel dizzy and I have a slight headache now and then',
+  'I feel dizzy and I have a slight headache now and then',
+];
+
+const MainChat = ({darkMode}) => {
+
+  const [isShown, setIsShown] = useState(false)
+
+  const handleModal = () => {
+    setIsShown(!isShown)
+  }
+
   return (
     <section className='px-6 pt-5 w-full'>
       <header className='flex justify-between items-center mt-1 lg:mt-24'>
@@ -17,13 +36,13 @@ const MainChat = () => {
 
       <div className='relative flex flex-col justify-center items-center'>
         <div className='w-fit flex justify-center relative'>
-        <img src={mainchatlogo} className='w-72 lg:w-full' />
-        <div className='font-AeonikTRAIL_Regular bg-[#D9F3EA] p-4 rounded-b-[1.8rem] rounded-l-[1.8rem] text-textprimary text-[0.6rem] absolute top-36 -left-8 shadow-xl  shadow-l-xl sm:-left-8 md:p-4 md:-left-12 lg:text-[1.1rem] lg:-left-16  lg:top-52'>
-        <p>Hi, John Doe, how are you</p>
-        <p>feeling today? Below are</p>
-        <p>a few common symptoms</p>
-        <p>that you might be feeling</p>
-      </div>
+          <img src={mainchatlogo} className='w-72 lg:w-full' />
+          <div className='font-AeonikTRAIL_Regular bg-[#D9F3EA] p-4 rounded-b-[1.8rem] rounded-l-[1.8rem] text-textprimary text-[0.6rem] absolute top-36 -left-8 shadow-xl  shadow-l-xl sm:-left-8 md:p-4 md:-left-12 lg:text-[1.1rem] lg:-left-16  lg:top-52'>
+            <p>Hi, John Doe, how are you</p>
+            <p>feeling today? Below are</p>
+            <p>a few common symptoms</p>
+            <p>that you might be feeling</p>
+          </div>
        </div>
 
         <div className='font-AeonikTRAIL_Regular flex flex-row gap-6 w-full flex-wrap justify-center mb-24'>
