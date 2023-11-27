c2dbb7270adb20085366f4da7c3c29f1017c4ade
inyangj <inyangjohnson01@gmail.com>
2023-11-27 02:14:49 +0100

    mainchat

--- src/components/chatComponent/MainChat.jsx
+++ src/components/chatComponent/MainChat.jsx
@@ -6,6 +6,7 @@ import MessageBox from './MessageBox'
 import SideNav from './SideNav'
 import iconhead from '../../assets/iconhead.png'
 
+
 const data = [
   'I feel dizzy and I have a slight headache now and then',
   'I feel dizzy and I have a slight headache now and then',
@@ -16,7 +17,7 @@ const data = [
 ];
 
 const MainChat = ({darkMode}) => {
-  const { isOpen, openChat, closeChat } = useChat();
+
   const [isShown, setIsShown] = useState(false)
   const [userName, setUserName] = useState('');
 
@@ -43,31 +44,6 @@ const MainChat = ({darkMode}) => {
         <div></div>
       </header>
 
-      <div className='relative flex flex-col justify-center items-center'>
-        <div className='w-fit flex justify-center relative'>
-          <img src={mainchatlogo} className='w-72 lg:w-full' />
-          <div className='font-AeonikTRAIL_Regular bg-[#D9F3EA] p-4 rounded-b-[1.8rem] rounded-l-[1.8rem] text-textprimary text-[0.6rem] absolute top-36 -left-8 shadow-xl  shadow-l-xl sm:-left-8 md:p-4 md:-left-12 lg:text-[1.1rem] lg:-left-16  lg:top-52'>
-            <p>Hi, {userName}, how are you</p>
-            <p>feeling today? Below are</p>
-            <p>a few common symptoms</p>
-            <p>that you might be feeling</p>
-          </div>
-       </div>
-
-        <div className='font-AeonikTRAIL_Regular flex flex-row gap-6 w-full flex-wrap justify-center mb-24'>
-        {/* so here we are showing possible chat symptoms for the potential users */}
-          {data.map((text, index) => (
-            <p  key={index}
-                className={`${darkMode ? 'bg-[#333]' : 'bg-[rgb(246,247,248)]'}
-                ${darkMode ? 'text-white' : 'text-[#282828]'} 
-                w-[21rem] px-2 py-3 rounded-xl font-AeonikTRAIL_Regular`}>
-              {text}
-            </p>
-          ))}
-        </div>
-        
-
-      </div>
      
       <MessageBox />
     </section>
--- src/utility/ChatApi.jsx
+++ src/utility/ChatApi.jsx
@@ -6,13 +6,13 @@ const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 const getAuthorizationHeader = () => {
   const userData = JSON.parse(localStorage.getItem("userData"));
   return {
-    Authorization: `Bearer ${userData.token}`,
+    Authorization: `Bearer ${userData.accessToken}`,
   };
 };
 
-export async function createChat(chat) {
+export async function createChat(chatData) {
     const headers = getAuthorizationHeader();
-    return await axios.post(` ${BASE_URL}/api/v1/messages`, chat, {
+    return await axios.post(` ${BASE_URL}/api/v1/messages`, chatData, {
       headers,
     });
   }
